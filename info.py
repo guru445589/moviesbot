b81b5528635cb60565170732b4cdaa13a6547e28
guru445589 <gurupodcast88@gmail.com>
2025-05-07 14:33:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7264137466:AAF1MPi_wftXjoQPNUX3xERdDTJqWLF
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1278626587').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002593074176')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002593074176'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviesprover')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002619448981').split()]
 #---------------------------------------------------------------
